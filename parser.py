8162134091952f67686c894fe50fd3518ea1d291
smngrvn <simon.grevin@gmail.com>
2016-10-26 12:16:36 +0000

    improving parser.py, writing results to csv

--- parser.py
+++ parser.py
@@ -1,13 +1,16 @@
 from bs4 import BeautifulSoup
 import urllib
 
-#for i in range(1,28):
+for i in range(1,28):
+    r = urllib.urlopen('https://www.marathon-toulousemetropole.fr/fr/resultats?type=&annee=&nom=&prenom=&club=&dossard=&sexe=&categorie=&p='+str(i)).read()
+    soup = BeautifulSoup(r, "html5lib")
+    rows = soup.find_all('table')[0].find_all('tr')
+    file = open('result.txt', 'a')
+    for tr_soup in rows:
+        col = tr_soup.find_all('td')
+        if len(col) > 0:
+            print(col[0].getText() + ";" + col[1].getText() + ";" + col[2].getText() + ";" + col[4].getText() + ";\n")
+            line = col[0].getText() + ";" + col[1].getText() + ";" + col[2].getText() + ";" + col[4].getText() + ";\n"
+            file.write(line.encode('utf-8'))
 
-
-r = urllib.urlopen('https://www.marathon-toulousemetropole.fr/fr/resultats?type=&annee=&nom=&prenom=&club=&dossard=&sexe=&categorie=&p=1').read()
-soup = BeautifulSoup(r, "html5lib")
-rows = soup.find_all('table')[0].find_all('tr')
-for tr_soup in rows:
-    col = tr_soup.find_all('td')
-    if len(col) > 0:
-        print("Nom : " + col[2].getText())
+file.close();
