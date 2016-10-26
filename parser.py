b9400ba56515ccc0c7eacf4e63dea2a72bd67e80
smngrvn <simon.grevin@gmail.com>
2016-10-26 12:23:02 +0000

    adding comments and result file

--- parser.py
+++ parser.py
@@ -1,16 +1,30 @@
 from bs4 import BeautifulSoup
 import urllib
 
+# result file
+file = open('marathon_results.csv', 'a')
+
+# loop on all pages
 for i in range(1,28):
-    r = urllib.urlopen('https://www.marathon-toulousemetropole.fr/fr/resultats?type=&annee=&nom=&prenom=&club=&dossard=&sexe=&categorie=&p='+str(i)).read()
-    soup = BeautifulSoup(r, "html5lib")
+    
+    # open url
+    url = urllib.urlopen('https://www.marathon-toulousemetropole.fr/fr/resultats?type=&annee=&nom=&prenom=&club=&dossard=&sexe=&categorie=&p='+str(i)).read()
+    soup = BeautifulSoup(url, "html5lib")
+    
+    # get all the rows
     rows = soup.find_all('table')[0].find_all('tr')
-    file = open('result.txt', 'a')
+    
+    # for each row
     for tr_soup in rows:
+        
+        # get all the cols
         col = tr_soup.find_all('td')
+        
         if len(col) > 0:
-            print(col[0].getText() + ";" + col[1].getText() + ";" + col[2].getText() + ";" + col[4].getText() + ";\n")
+            # getting only the relevant info : spot;bib number;name;time
             line = col[0].getText() + ";" + col[1].getText() + ";" + col[2].getText() + ";" + col[4].getText() + ";\n"
+            
+            # writing to result file
             file.write(line.encode('utf-8'))
 
 file.close();
