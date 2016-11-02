b6415e43cf52363d9ba8a2d7597918d03c11220c
smngrvn <simon.grevin@gmail.com>
2016-11-02 22:00:38 +0100

    gender category impossible with 2016 data

--- marathon_stats.py
+++ marathon_stats.py
@@ -10,12 +10,6 @@ def get_age_category(x):
     if x[0] == 'V':
         return 'Veteran'
 
-# get sex category based on first letter
-def get_gender_category(x):
-    if x[-1:] == 'F':
-        return 'Femme'
-    if x[-1:] == 'H':
-        return 'Homme'
 
 # read csv
 df = pd.read_csv("marathon_2016_results.csv", delimiter=";")
@@ -24,11 +18,5 @@ df = pd.read_csv("marathon_2016_results.csv", delimiter=";")
 age_category_dataframe = df['category'].map(get_age_category)
 final_df = df.join(age_category_dataframe, rsuffix='_age')
 
-# create new dataframe for gender category and join
-gender_category_dataframe = df['category'].map(get_gender_category)
-final_df = final_df.join(gender_category_dataframe, rsuffix='_gender')
-
-
 # print final dataframe grouped by age category then by gender
 print(final_df.groupby(['category_age'])).count()
-print(final_df.groupby(['category_gender'])).count()
